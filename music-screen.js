fe10f0fdfdabccdc469f765e36572de7d605b4a3
Nian PC <yt8956789@gmail.com>
2019-05-17 17:11:19 +0800

    fix bug of playing music automatically

--- music-screen.js
+++ music-screen.js
@@ -13,21 +13,29 @@ class MusicScreen {
     this.musicScreenInit = this.musicScreenInit.bind(this);
     this.onClick = this.onClick.bind(this);
     this.show = this.show.bind(this);
-    let playButtonElement = document.getElementById("controlButton");
+    this.playButtonElement = document.getElementById("controlButton");
 
-    this.audioPlayer = new AudioPlayer();
-    this.playButton = new PlayButton(playButtonElement);
+    this.audioPlayer = 0;
+    this.playButton = new PlayButton(this.playButtonElement);
     this.gifDisplay = null;
     this.NowShowPause=false;
+    this.songUrl = null;
 
     this.hide();
     document.addEventListener("toMusic", this.musicScreenInit);
-
-    playButtonElement.onclick = this.onClick;
-    console.log(playButtonElement);
+    this.playButtonElement.addEventListener("autoPlayMusic", this.onClick);
+    this.playButtonElement.onclick = this.onClick;
   }
 
   onClick(){
+
+    if(this.audioPlayer===0){
+      this.audioPlayer = new AudioPlayer();
+      this.audioPlayer.setSong(this.songUrl);
+      this.audioPlayer.setKickCallback(()=>{document.dispatchEvent(new Event("onKick"));});
+      console.log("AudioPlayer Init")
+    }
+
     if(this.NowShowPause)
     {
       this.NowShowPause = false;
@@ -55,8 +63,7 @@ class MusicScreen {
     let json=event.detail.json;
     let imageUrl=[];
 
-    this.audioPlayer.setSong(event.detail.songUrl);
-    this.audioPlayer.setKickCallback(()=>{document.dispatchEvent(new Event("onKick"));});
+    this.songUrl=event.detail.songUrl;
 
     for(let key in json.data)
     {
@@ -67,5 +74,6 @@ class MusicScreen {
 
     this.gifDisplay = new GifDisplay(imageUrl);
     this.show();
+    this.playButtonElement.dispatchEvent(new Event("autoPlayMusic"));
   }
 }
